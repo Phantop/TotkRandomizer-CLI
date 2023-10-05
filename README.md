b8eb7c99d129aaecc1ef56d44fdcc603b13e6016
Phantop <phantop@tuta.io>
2023-10-05 19:02:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# TOTK Randomizer Mod
+A port of [MelonSpeedruns's](https://github.com/MelonSpeedruns/)
+[Tears of the Kingdom Randomizer](https://github.com/MelonSpeedruns/TotkRandomizer)
+to a commandline utility that can run on Linux with no changes to the original source.
+
+To run:
+```sh
+cd src
+./setup.sh
+dotnet run -c Release <path/to/romfs>
+```
+* romfs path must not end in a `/`
---
+++ src/Program.cs
@@ -0,0 +1,34 @@
+namespace TotkRandomizer {
+    public class Application {
+        static void Main(string[] args) {
+            if (args.Length > 0) { textBox1.Text = args[0]; Form1 f = new Form1(); }
+        }
+    }
+    public partial class Form1 : Form {
+        private void InitializeComponent() {
+            button1_Click(null, null); backgroundWorker1_DoWork(null, null); }
+        public static int getMax() { return maxProgress; } }
+    public static class backgroundWorker1 {
+        public static void RunWorkerAsync(){}
+        public static void ReportProgress(int i) { Console.WriteLine(i + "/" + Form1.getMax()); } }
+    public class FolderBrowserDialog {
+        public bool AutoUpgradeEnabled, UseDescriptionForTitle;
+        public bool ShowDialog() { return true; }
+        public string Description, SelectedPath; }
+    public class NLM {
+        public static NLM RegisterAssembly(System.Reflection.Assembly a, out bool b) { b = true; return new NLM(); }
+        public void Register(CsRestbl.RestblLibrary a, out bool b) { b = false; } }
+    public class Form{}
+    public class NativeLibraryManager : NLM{};
+    public static class DialogResult { public static bool OK; }
+    public static class button1 { public static bool Enabled; }
+    public static class button2 { public static bool Enabled; }
+    public static class progressBar1 { public static int Maximum, Value; }
+    public static class textBox1 { public static string Text; }
+    namespace SystemSounds {
+        public static class Exclamation { public static void Play(){} }}
+    namespace Properties { namespace Settings {
+        public static class Default {
+            public static string totkPath;
+            public static void Save(){} }}}
+}
